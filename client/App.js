c4ad84d142fabfdea976968565e89fc6cf7b96e7
Marvin Liu <superliuwr@gmail.com>
2017-10-31 22:37:58 +1100

    Enable Apollo client

--- client/App.js
+++ client/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+
+// Initialize Apollo client
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' });
+const client = new ApolloClient({
+  networkInterface,
+});
+
+// Initialize Redux store
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer(),
+  }),
+  {},
+  composeWithDevTools(
+    applyMiddleware(client.middleware()),
+  ),
+);
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-      </View>
+      <ApolloProvider store={store} client={client}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
