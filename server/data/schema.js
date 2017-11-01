a54d4444400c512257962dd8baccaffb7d070c81
Marvin Liu <superliuwr@gmail.com>
2017-11-01 22:46:27 +1100

    Add Graphql schema and mock

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,11 +1,41 @@
-export const Schema = [
-  `type Query {
-      testString: String
+export const Schema = [`
+  scalar Date
+
+  type Group {
+    id: Int!
+    name: String
+    users: [User]!
+    messages: [Message]
+  }
+
+  type User {
+    id: Int!
+    email: String!
+    username: String
+    messages: [Message]
+    groups: [Group]
+    friends: [User]
+  }
+
+  type Message {
+    id: Int!
+    to: Group!
+    from: User!
+    text: String!
+    createdAt: Date!
+  }
+
+  type Query {
+    user(email: String, it: Int): User
+
+    messages(groupId: Int, userId: Int): [Message]
+
+    group(id: Int!): Group
   }
 
   schema {
-      query: Query
-  }`,
-];
+    query: Query
+  }
+`];
 
 export default Schema;
